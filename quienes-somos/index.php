82322b6f196224fc3c2cd017cf9070e2226841b9
Laura Cabrera <lzcabrera@gmail.com>
2011-12-12 23:49:40 -0800

    quienes somos markup update

--- quienes-somos/index.php
+++ quienes-somos/index.php
@@ -44,7 +44,9 @@
 
 <h2>Nuestra calificación internacional</h2>
 
-<p>Somos miembros de <a href="http://www.agn.org/">AGN Internacional</a>, una asociación mundial de firmas independientes de auditoria y consultoría contable y financiera con 195 firmas  que operan en más de 89 países, con 455 oficinas integradas por 8,800 socios y personal, estamos entre las 12 firmas de auditoria más importantes en los respectivos países. Las firmas miembros de AGN ayudamos a nuestros clientes a enfrentar los desafíos que representa el hecho de realizar transacciones y negocios nacionales e internacionales y les asesoramos en materia financiera y tributaria.  A través de AGN, nuestros clientes tienen acceso a una red mundial de asesores profesionales que les podrán dar respuesta a cualquier consulta en todo momento.  Las evaluaciones internas que nuestra organización realiza periódicamente a los procedimientos utilizados por sus miembros son una garantía de un nivel de calidad frente a nuestros clientes.</p>
+<p>Somos miembros de <a href="http://www.agn.org/">AGN Internacional</a>, una asociación mundial de firmas independientes de auditoria y consultoría contable y financiera con 195 firmas  que operan en más de 89 países, con 455 oficinas integradas por 8,800 socios y personal, estamos entre las 12 firmas de auditoria más importantes en los respectivos países.
+	<p> Las firmas miembros de AGN ayudamos a nuestros clientes a enfrentar los desafíos que representa el hecho de realizar transacciones y negocios nacionales e internacionales y les asesoramos en materia financiera y tributaria.  </p>
+	<p>A través de AGN, nuestros clientes tienen acceso a una red mundial de asesores profesionales que les podrán dar respuesta a cualquier consulta en todo momento.  Las evaluaciones internas que nuestra organización realiza periódicamente a los procedimientos utilizados por sus miembros son una garantía de un nivel de calidad frente a nuestros clientes.</p>
 
 
 </div>
