26621334f446bb3f854442dc3f3abd505865bd2a
Laura Cabrera <lzcabrera@gmail.com>
2012-07-02 10:32:35 -0700

    copy review with dad

--- servicios/index.php
+++ servicios/index.php
@@ -3,7 +3,7 @@
 		
 <div class="middle-column">
 	<div id="content">
-	<p>Los servicios que principalmente prestamos, entre otros, son: <a href="/servicios/auditoria-externa">auditoría externa</a>, <a href="/servicios/auditoria-fiscal">auditoría fiscal</a>, <a href="/servicios/auditoria-interna">auditoría interna</a>, servicios de <a href="/servicios/due-dilligence">due dilligence</a>, servicios <a href="/servicios/especiales-previamente-acordados">especiales sobre procedimientos previamente acordados</a>, <a href="/servicios/impuestos">impuestos</a>, <a href="/servicios/legalizacion-sistemas-contables">legalización de sistemas contables</a> y <a href="/servicios/autorizacion-registros-contables">autorización de registros contables</a>.</p>
+	<p>Los servicios que prestamos, entre otros, son: <a href="/servicios/auditoria-externa">auditoría externa</a>, <a href="/servicios/auditoria-fiscal">auditoría fiscal</a>, <a href="/servicios/auditoria-interna">auditoría interna</a>, servicios <a href="/servicios/especiales-previamente-acordados">especiales sobre procedimientos previamente acordados</a>, <a href="/servicios/impuestos">impuestos</a>, <a href="/servicios/legalizacion-sistemas-contables">legalización de sistemas contables</a> y <a href="/servicios/autorizacion-registros-contables">autorización de registros contables</a>.</p>
 
 	<p>Para mas información acerca de cada uno de estos servicios visite los enlaces en el menu de la izquierda.</p>
 	</div>
--- servicios/legalizacion-sistemas-contables/index.php
+++ servicios/legalizacion-sistemas-contables/index.php
@@ -2,7 +2,7 @@
 		
 <div class="middle-column">
 	<div id="content">
-		<p>Proporcionamos los servicios de autorización de sistemas contables, revisando los sistemas contables actualmente en uso, para su modificación y asesoría en la elaboración de sistemas contables que respondan a las exigencias de las Normas Internacionales de Información Financiera – NIIF (antes Normas Internacionales de Contabilidad – NIC) vigentes a partir de enero de 2004, en forma escalonada según acuerdo respectivo.</p>
+		<p>Proporcionamos los servicios de autorización de sistemas contables, revisando los sistemas contables actualmente en uso, para su modificación y asesoría en la elaboración de sistemas contables que respondan a las exigencias de las Normas Internacionales de Información Financiera – NIIF (antes Normas Internacionales de Contabilidad – NIC) vigentes a partir de enero de 2011, en forma escalonada según acuerdo respectivo.</p>
 	</div>
 </div>
 
