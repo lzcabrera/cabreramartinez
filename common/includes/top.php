dfbfbd5e65fad5871092b68bab59508c2b9e8578
Laura Cabrera <lzcabrera@gmail.com>
2012-07-06 19:24:50 -0700

    favicon fix

--- common/includes/top.php
+++ common/includes/top.php
@@ -33,11 +33,11 @@
 	<meta name="description" content="<?php echo $page_meta_description; ?>" />
 	<meta name="keywords" content="Cabrera Martinez S.A. de C.V., Mario Cabrera, Mario Humberto Cabrera Martinez, Cabrera Martínez San Salvador, El Salvador, Cabrera Martinez El Salvador, Auditoria en El Salvador, San Salvador Auditoria, San Salvador Auditoría, Expertos en Auditoria en El Salvador, AGN El Salvador, Contaduria Publica San Salvador, Auditores en San Salvador, Contaduría Pública en El Salvador" />
 	<meta name="google-site-verification" content="HSXE-XlLPj0krD0_6Mszi-icF6NvgAT1OpWu-OXPh-U" />
-	<meta name="author" content="funnelcity.com">
-	<link type="image/x-icon" href="/img/favicon.ico" rel="shortcut icon">
+	<meta name="author" content="funnelcity.com"/>
+	<link type="image/x-icon" href="/img/favicon.ico" rel="icon"/>
 	
 	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
-	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="viewport" content="width=device-width,initial-scale=1"/>
 	<meta name="google-site-verification" content="NkpvNaNpcs9kQ6NoZC9tCGE5CewBaIukY6ms5zbUsUY" />
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 		
